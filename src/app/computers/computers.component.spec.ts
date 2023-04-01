65dd8f3af1321b6ff7c72233149f9554eb1d34aa
Luis Leonardo Fuentes Carmona <lleofuca@gmail.com>
2023-04-01 00:23:03 -0600

    Pruebas unitarias computers

--- src/app/computers/computers.component.spec.ts
+++ src/app/computers/computers.component.spec.ts
@@ -3,10 +3,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ComputersComponent } from './computers.component';
 import { ComputerService } from '../services/computer.service';
 import { MatTable, MatTableModule } from '@angular/material/table';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Computer } from '../model/computer.model';
 
 describe('ComputersComponent', () => {
   let component: ComputersComponent;
@@ -37,5 +38,103 @@ describe('ComputersComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should loadData ok`, () => {
+    const mockResponse: Computer[] = [
+      {
+        id: 1,
+        brand: 'Lenovo',
+        model: 'H1M1',
+      },
+      {
+        brand: 'HP',
+        model: 'Pavilion',
+        id: 2,
+      },
+    ];
+
+    computersrvSpy.getComputers.and.returnValue(of(mockResponse));
+
+    component.loadData();
+
+    expect(component.computers.data.length).toBe(2);
+  });
+
+  it(`should loadData error`, () => {
+    const mockResponseEmpty: Computer[] = [];
+    computersrvSpy.getComputers.and.returnValue(of(mockResponseEmpty));
+    component.loadData();
+
+    expect(component.computers.data.length).toBe(0);
+
+    computersrvSpy.getComputers.and.returnValue(
+      throwError(() => 'error, list is empty')
+    );
+  });
+
+  it('should deleteComputer ok', () => {
+    const mockResponse: Computer[] = [
+      {
+        id: 1,
+        brand: 'Lenovo',
+        model: 'H1M1',
+      },
+      {
+        brand: 'HP',
+        model: 'Pavilion',
+        id: 2,
+      },
+    ];
+
+    computersrvSpy.getComputers.and.returnValue(of(mockResponse));
+
+    component.loadData();
+
+    const computer: Computer = {
+      id: 1,
+      brand: 'Lenovo',
+      model: 'H1M1',
+    };
+
+    mockResponse.shift();
+    computersrvSpy.deleteComputers.and.returnValue(of(mockResponse));
+
+    component.deleteComputer(computer);
+
+    expect(component.computers.data.length).toBe(1);
+  });
+
+  it('should deleteComputer error', () => {
+    const mockResponse: Computer[] = [
+      {
+        id: 1,
+        brand: 'Lenovo',
+        model: 'H1M1',
+      },
+      {
+        brand: 'HP',
+        model: 'Pavilion',
+        id: 2,
+      },
+    ];
+
+    computersrvSpy.getComputers.and.returnValue(of(mockResponse));
+
+    component.loadData();
+
+    const computer: Computer = {
+      id: 1,
+      brand: 'Lenovo',
+      model: 'H1M1',
+    };
+
+    computersrvSpy.deleteComputers.and.returnValue(
+      throwError(() => 'error deleting')
+    );
+
+    component.deleteComputer(computer);
+
+    expect(component.computers.data.length).toBe(2);
   });
 });
--- src/app/computers/new-computer/new-computer.component.spec.ts
+++ src/app/computers/new-computer/new-computer.component.spec.ts
@@ -1,11 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component } from '@angular/core';
 import { NewComputerComponent } from './new-computer.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ComputerService } from 'src/app/services/computer.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { of, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 describe('NewComputerComponent', () => {
   let component: NewComputerComponent;
@@ -14,6 +16,8 @@ describe('NewComputerComponent', () => {
     'ComputerService',
     ['saveComputers']
   );
+
+  let routerSpy = jasmine.createSpyObj<Router>('Router', ['navigate']);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -34,5 +38,18 @@ describe('NewComputerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should save Computer', () => {
+    computersrvSpy.saveComputers.and.returnValue(of([]));
+
+    component.formComputer?.setValue({
+      brand: 'Lenovo',
+      model: 'H1M1',
+    });
+
+    // component.saveComputer();
+
+    // expect(routerSpy.navigate).toHaveBeenCalledWith(['/computers']);
   });
 });
